3d8c1e3de3c17f0142e01ca0c44284112567bb58
0xShisui <90075884+0xShisui@users.noreply.github.com>
2022-03-10 15:35:31 +0700

    chore: Update release options

--- .release-it.js
+++ .release-it.js
@@ -2,6 +2,7 @@
 module.exports = {
   git: {
     commitMessage: "build: Release v${version}",
+    requireUpstream: false,
     pushRepo: "upstream", // Push tags and commit to the remote `upstream` (fails if doesn't exist)
     requireBranch: "master", // Push commit to the branch `master` (fail if on other branch)
     requireCommits: true, // Require new commits since latest tag
