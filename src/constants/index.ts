ce439162b2036a5bbd41e5011e9c2265a98ef2f8
Shisui <90075884+0xShisui@users.noreply.github.com>
2022-05-13 17:01:10 +0900

    chore: Add constants and types (#37)

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,6 @@
+// This variable is used to enforce a maximum slippage of 15% on orders.
+// Orders with higher slippage will become invalid.
+export const minNetPriceRatio = 8500;
+
 export { addressesByNetwork } from "./addresses";
 export { CHAIN_INFO, isSupportedChain } from "./chains";
