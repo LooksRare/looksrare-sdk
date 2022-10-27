3e7db207b810bf5616067dea91d5e55c348c093f
0xShisui <90075884+0xShisui@users.noreply.github.com>
2022-10-27 17:33:44 +0800

    chore: Update minNetPriceRatio

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,5 @@
-// This variable is used to enforce a maximum slippage of 25% on orders.
-// Orders with higher slippage will become invalid.
-export const minNetPriceRatio = 7500;
+export const minNetPriceRatio = 9800;
+export const minNetPriceRatioDeprecated = 9750;
 
 export { addressesByNetwork } from "./addresses";
 export { CHAIN_INFO, isSupportedChain } from "./chains";
