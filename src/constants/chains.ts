bfc5111883460d5311568f2cc7f2eadf69b1330f
BrooceLR <brooce@looksrare.org>
2022-12-12 17:40:27 +0100

    refactor(chains): Add baseApiUrl to CHAIN_INFO
    Adds `baseApiUrl` to each `CHAIN_INFO` and creates `apiUrl` by extending that

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,39 +1,51 @@
 import { ChainInfo, SupportedChainId } from "../types";
 
 export const CHAIN_INFO: { [chainId in SupportedChainId]: ChainInfo } = {
-  [SupportedChainId.MAINNET]: {
-    label: "Ethereum",
-    appUrl: "https://looksrare.org",
-    explorer: "https://etherscan.io",
-    rpcUrl: `https://eth-mainnet.g.alchemy.com/v2`,
-    apiUrl: "https://graphql.looksrare.org/graphql",
-    osApiUrl: "https://api.opensea.io",
-    cdnUrl: "https://static.looksnice.org",
-    rewardsSubgraphUrl: "https://api.thegraph.com/subgraphs/name/looksrare/looks-distribution",
-    cloudinaryUrl: "https://looksrare.mo.cloudinary.net",
-  },
-  [SupportedChainId.GOERLI]: {
-    label: "Goerli",
-    appUrl: "https://goerli.looksrare.org",
-    explorer: "https://goerli.etherscan.io",
-    rpcUrl: `https://eth-goerli.g.alchemy.com/v2`,
-    apiUrl: "https://graphql-goerli.looksrare.org/graphql",
-    osApiUrl: "https://testnets-api.opensea.io",
-    cdnUrl: "https://static-goerli.looksnice.org",
-    rewardsSubgraphUrl: "https://api.thegraph.com/subgraphs/name/0xjurassicpunk/looks-distribution",
-    cloudinaryUrl: "https://looksrare.mo.cloudinary.net/goerli",
-  },
-  [SupportedChainId.HARDHAT]: {
-    label: "Hardhat",
-    appUrl: "http://localhost:3000",
-    explorer: "https://etherscan.io",
-    rpcUrl: "http://127.0.0.1:8545",
-    apiUrl: "http://localhost:4000/graphql",
-    osApiUrl: "https://testnets-api.opensea.io",
-    cdnUrl: "https://via.placeholder.com",
-    rewardsSubgraphUrl: "https://api.thegraph.com/subgraphs/name/0xjurassicpunk/looks-distribution",
-    cloudinaryUrl: "",
-  },
+  [SupportedChainId.MAINNET]: (()=> {
+    const baseApiUrl = "https://graphql.looksrare.org"
+    return {
+      label: "Ethereum",
+      appUrl: "https://looksrare.org",
+      explorer: "https://etherscan.io",
+      rpcUrl: `https://eth-mainnet.g.alchemy.com/v2`,
+      baseApiUrl,
+      apiUrl: `${baseApiUrl}/graphql`,
+      osApiUrl: "https://api.opensea.io",
+      cdnUrl: "https://static.looksnice.org",
+      rewardsSubgraphUrl: "https://api.thegraph.com/subgraphs/name/looksrare/looks-distribution",
+      cloudinaryUrl: "https://looksrare.mo.cloudinary.net",
+    }
+  })(),
+  [SupportedChainId.GOERLI]: (()=> {
+    const baseApiUrl = "https://graphql-goerli.looksrare.org"
+    return {
+      label: "Goerli",
+          appUrl: "https://goerli.looksrare.org",
+        explorer: "https://goerli.etherscan.io",
+        rpcUrl: `https://eth-goerli.g.alchemy.com/v2`,
+        baseApiUrl,
+        apiUrl: `${baseApiUrl}/graphql`,
+        osApiUrl: "https://testnets-api.opensea.io",
+        cdnUrl: "https://static-goerli.looksnice.org",
+        rewardsSubgraphUrl: "https://api.thegraph.com/subgraphs/name/0xjurassicpunk/looks-distribution",
+        cloudinaryUrl: "https://looksrare.mo.cloudinary.net/goerli",
+    }
+  })(),
+  [SupportedChainId.HARDHAT]: (()=> {
+    const baseApiUrl = "http://localhost:4000"
+    return {
+      label: "Hardhat",
+      appUrl: "http://localhost:3000",
+      explorer: "https://etherscan.io",
+      rpcUrl: "http://127.0.0.1:8545",
+      baseApiUrl,
+      apiUrl: `${baseApiUrl}/graphql`,
+      osApiUrl: "https://testnets-api.opensea.io",
+      cdnUrl: "https://via.placeholder.com",
+      rewardsSubgraphUrl: "https://api.thegraph.com/subgraphs/name/0xjurassicpunk/looks-distribution",
+      cloudinaryUrl: "",
+    }
+  })(),
 };
 
 export const isSupportedChain = (chainId: number): chainId is SupportedChainId => {
