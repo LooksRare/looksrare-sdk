557828b91a8b07742a9ec6f47a7f182a1e3ead80
Nirato <nirato@looksrare.org>
2023-01-19 15:27:53 -0600

    build: Transpile TS ABI exports

--- rollup.config.js
+++ rollup.config.js
@@ -3,20 +3,27 @@ import json from "@rollup/plugin-json";
 import copy from "rollup-plugin-copy";
 import pkg from "./package.json";
 
-export default {
-  input: "src/index.ts",
-  output: [
-    { file: pkg.main, format: "cjs" },
-    { file: pkg.module, format: "es" },
-  ],
-  plugins: [
-    copy({
-      targets: [
-        { src: "src/abis/json/**/*", dest: "dist/abis/json" },
-        { src: "src/abis/ts/**/*", dest: "dist/abis/ts" },
-      ],
-    }),
-    json(),
-    typescript(),
-  ],
-};
+export default [
+  {
+    input: "src/index.ts",
+    output: [
+      { file: pkg.main, format: "cjs" },
+      { file: pkg.module, format: "es" },
+    ],
+    plugins: [
+      copy({
+        targets: [{ src: "src/abis/json/**/*", dest: "dist/abis/json" }],
+      }),
+      json(),
+      typescript(),
+    ],
+  },
+  {
+    input: "src/abis/ts/index.ts",
+    output: [
+      { file: "dist/abis/ts/index.esm.js", format: "es" },
+      { file: "dist/abis/ts/index.cjs.js", format: "cjs" },
+    ],
+    plugins: [typescript()],
+  },
+];
